1b6a32d9ec7e82ff1b5077ae4a014f78ff1ade2f
rose-okoth <okoth.rose0@gmail.com>
2021-03-06 11:46:41 +0300

    initialized database and added in the user model

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='123456'
+export MAIL_USERNAME='schoolprojcts3@gmail.com'
+export MAIL_PASSWORD='m3stravaill3s'
+export DATABASE_URL='postgresql+psycopg2://oem:m3stravaill3s@localhost/blog'
+python3 manage.py server
