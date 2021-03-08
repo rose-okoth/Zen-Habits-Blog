f803bdb485e3073970bb84148d0a6911dedcf827
rose-okoth <okoth.rose0@gmail.com>
2021-03-08 13:23:10 +0300

    edited files

--- start.sh
+++
@@ -1,5 +0,0 @@
-export SECRET_KEY='123456'
-export MAIL_USERNAME='schoolprojcts3@gmail.com'
-export MAIL_PASSWORD='m3stravaill3s'
-export DATABASE_URL='postgresql+psycopg2://oem:m3stravaill3s@localhost/blog1'
-python3 manage.py server
